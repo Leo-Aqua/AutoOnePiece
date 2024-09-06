d81d12c4a419dbed09899a0fe6c04d46aa298e00
Leo Aqua <88901298+Leo-Aqua@users.noreply.github.com>
2024-09-06 13:32:59 +0200

    Added Threading

--- main.py
+++ main.py
@@ -1,101 +1,102 @@
 from youtubesearchpython import VideosSearch, Transcript
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtCore import QUrl, Qt
+from PySide6.QtCore import QUrl, Qt, QThread, Signal
 from PySide6.QtGui import QPalette, QColor, QDesktopServices
 from ui_mainwindow import Ui_MainWindow
 
 
+class SearchWorker(QThread):
+    progress = Signal(int)
+    status = Signal(str)
+    result = Signal(list)
+    finished = Signal()
+
+    def __init__(self, ytsearchterm, searchterm, maxresults):
+        super().__init__()
+        self.ytsearchterm = ytsearchterm
+        self.searchterm = searchterm
+        self.maxresults = maxresults
+
+    def run(self):
+        results = []
+        self.status.emit("Searching...")
+        s = VideosSearch(query=self.ytsearchterm, limit=self.maxresults, language="en")
+        results = s.result()["result"]
+
+        pages = self.maxresults / len(results)
+        if not pages.is_integer():
+            pages = int(pages + 1)
+
+        for i in range(int(pages)):
+            self.progress.emit(int(((i + 1) / pages) * 100))
+            s.next()
+            results += s.result()["result"]
+
+        if len(results) > self.maxresults:
+            results = results[: self.maxresults]
+
+        self.status.emit("Downloading and parsing subtitles...")
+        filtered_results = []
+        for i, video in enumerate(results):
+            self.progress.emit(int(((i + 1) / len(results)) * 100))
+            subtitles = self.download_subtitles(video["id"])
+            if subtitles:
+                timestamps = self.parse_subtitles(video["id"], subtitles)
+                if timestamps:
+                    filtered_results.extend(timestamps)
+
+        self.result.emit(filtered_results)
+        self.finished.emit()
+
+    def download_subtitles(self, video_id):
+        url = f"https://www.youtube.com/watch?v={video_id}"
+        try:
+            return Transcript.get(url)["segments"]
+        except:
+            return None
+
+    def parse_subtitles(self, video_id, subtitles):
+        timestamps = []
+        for segment in subtitles:
+            if self.searchterm.lower() in segment["text"].lower():
+                timestamp = f"https://youtu.be/{video_id}?t={segment['startMs'][:-3]}"
+                timestamps.append(timestamp)
+        return timestamps
+
+
 class MainWindow(QMainWindow):
-
     def __init__(self):
-
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.results = None
-
-        self.ui.searchButton.clicked.connect(self.main)
+        self.ui.searchButton.clicked.connect(self.start_search)
         self.ui.listWidget.itemDoubleClicked.connect(self.open_listbox_link)
+        self.setFixedSize(639, 336)
+        self.search_worker = None
 
-    def main(self):
-
-        self.ytsearchterm: str = self.ui.ytSearchInput.text()
-        self.searchterm: str = self.ui.searchTermInput.text()
-        self.maxresults: int = self.ui.maxSearchSpinBox.value()
-
+    def start_search(self):
         self.ui.listWidget.clear()
+        self.ui.searchButton.setEnabled(False)
 
-        self.search()
-        print("Videos: " + str(len(self.results)))
+        ytsearchterm = self.ui.ytSearchInput.text()
+        searchterm = self.ui.searchTermInput.text()
+        maxresults = self.ui.maxSearchSpinBox.value()
 
-        for i, video in enumerate(self.results):
-            self.ui.statusLabel.setText("Downloading and parsing subtitles...")
-            self.ui.progressBar.setValue(((i + 1) / len(self.results)) * 100)
-            self.download_subtitles(video["id"])
-            self.parse_subtitles(video["id"])
+        self.search_worker = SearchWorker(ytsearchterm, searchterm, maxresults)
+        self.search_worker.progress.connect(self.ui.progressBar.setValue)
+        self.search_worker.status.connect(self.ui.statusLabel.setText)
+        self.search_worker.result.connect(self.update_list_widget)
+        self.search_worker.finished.connect(self.search_finished)
+        self.search_worker.start()
 
-    def search(self):
-        self.ui.statusLabel.setText("Searching...")
-        # Search for the video
-        s = VideosSearch(
-            query=self.ytsearchterm,
-            limit=self.maxresults,
-            language="en",
-        )
-        self.results = s.result()["result"]
+    def update_list_widget(self, results):
+        self.ui.listWidget.addItems(results)
 
-        pages = self.maxresults / len(self.results)
-        if pages.is_integer() == False:
-            pages = int(pages + 1)
-        for i in range(int(pages)):
-            print(i + 1)
-            print(pages)
-            print(((i + 1) / pages) * 100)
-
-            self.ui.progressBar.setValue(((i + 1) / pages) * 100)
-            s.next()
-            self.results += s.result()["result"]
-        n = len(self.results)
-        if n > self.maxresults:
-            for i in range(0, n - self.maxresults):
-                self.results.pop()
-
-    def download_subtitles(self, id):
-        url = "https://www.youtube.com/watch?v=" + id
-
-        try:
-            self.subtitles = Transcript.get(url)
-
-            self.subtitles = self.subtitles["segments"]
-        except:
-            pass
-
-    def parse_subtitles(self, id):
-        for segment in self.subtitles:
-
-            try:
-                if self.searchterm.lower() in segment["text"].lower():
-                    print(
-                        self.searchterm
-                        + " found in: "
-                        + "https://youtu.be/"
-                        + id
-                        + "?t="
-                        + segment["startMs"][:-3]
-                    )
-                    print(
-                        "start: "
-                        + str(segment["startMs"])
-                        + "\nend: "
-                        + str(segment["endMs"])
-                    )
-                    self.ui.listWidget.addItem(
-                        "https://youtu.be/" + id + "?t=" + segment["startMs"][:-3]
-                    )
-            except AttributeError:
-                pass
+    def search_finished(self):
+        self.ui.searchButton.setEnabled(True)
+        self.ui.statusLabel.setText("Search completed")
 
     def open_listbox_link(self):
         link = self.ui.listWidget.currentItem().text()
@@ -132,5 +133,6 @@ if __name__ == "__main__":
     app.setStyle("Fusion")
     app.setPalette(palette)
     window = MainWindow()
+
     window.show()
     sys.exit(app.exec())
